46d23a06ae1523ffcf2fb8e3a32595f2e62f77f0
yasinski yan <yasinskijan161@gmail.com>
2022-11-09 11:24:35 +0300

    asd

--- src/components/Account/Account.tsx
+++ src/components/Account/Account.tsx
@@ -1,3 +1,4 @@
+import { AxiosResponse } from "axios";
 import react, { useCallback, useEffect, useState } from "react";
 import { useTranslation } from "react-i18next";
 import { Outlet, useNavigate } from "react-router-dom";
@@ -12,32 +13,37 @@ import styles from "./styles.module.scss";
 
 const Account = () => {
   const navigate = useNavigate();
-
+  const [user, setUser] = useState<any>(null);
   const handleGo = useCallback(() => {
     navigate("/topics");
   }, [navigate]);
 
-  const {
-    setMainButtonOnClick,
-    setMainButtonParams,
-    setLoadingMainButton
-  } = useTelegramMainButton(true, true, "set visible");
+  const { setMainButtonOnClick, setMainButtonParams, setLoadingMainButton } =
+    useTelegramMainButton(true, true, "set visible");
 
   useTgBackButton(false);
 
-
-  const {t} = useTranslation();
+  const { t } = useTranslation();
 
   useEffect(() => {
-    setMainButtonOnClick(handleGo)
-  }, [handleGo, setMainButtonOnClick])
+    setMainButtonOnClick(handleGo);
+  }, [handleGo, setMainButtonOnClick]);
 
+  const makeRequest = useCallback(async () => {
+    fetch("https://shark-app-2zmo7.ondigitalocean.app/api/user/1222183809", {
+      method: "GET",
+      headers: { Authorization: `${window.Telegram.WebApp.initData}` },
+    }).then((response) => {
+      setUser(response);
+    })
+  }, []);
 
   return (
     <div className={styles.container}>
       <div>{window.Telegram.WebApp.initData}</div>
-      <h2>{t('test')}</h2>
+      <h2>{t("test")}</h2>
       <button onClick={handleGo}>go</button>
+      <button onClick={makeRequest}>request</button>
     </div>
   );
 };
